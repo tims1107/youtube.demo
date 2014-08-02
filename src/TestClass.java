73c3dcc6718e67e1f565041a0581aeafb2423473
tim.smithers <tim.smithers@developerinc.com>
2014-08-02 16:10:39 -0400

    more changes

--- src/TestClass.java
+++ src/TestClass.java
@@ -5,7 +5,7 @@ public class TestClass {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		System.out.println("this is a test");
+		System.out.println("this is a test / ok made this change");
 
 	}
 
